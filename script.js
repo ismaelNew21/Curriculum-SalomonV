3cc5bbc559ccac9bf1755ea5cd1007640324fe79
saymon <saymoncuro96@gmail.com>
2024-10-16 21:50:14 -0500

    Primer commit de mi cv

---
+++ script.js
@@ -0,0 +1,11 @@
+//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+// document.addEventListener("DOMContentLoaded", function() {
+//     const lista = document.querySelector('.habilidades__lista');
+//     let idx = 0;
+//     const totalItems = document.querySelectorAll('.habilidades__elemento').length;
+    
+//     setInterval(() => {
+//         idx = (idx + 1) % totalItems;
+//         lista.style.transform = `translateX(-${idx * 100}%)`;
+//     }, 3000); // Cambia cada 3 segundos
+// });
